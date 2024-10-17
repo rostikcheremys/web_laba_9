450110f0e802bcfbbc66629b609632088c96f5d7
rostik <rostikcheremys@gmail.com>
2024-10-17 19:30:27 +0300

    add ip_info_xml.php

---
+++ ip_info_xml.php
@@ -0,0 +1,12 @@
+﻿<?php
+$ip = $_POST['ip'];
+$xml_url = "http://ip-api.com/xml/{$ip}";
+
+$response = simplexml_load_file($xml_url);
+$xml_data = json_decode(json_encode($response), true);
+
+if (isset($xml_data['status']) && $xml_data['status'] == 'fail') {
+    echo json_encode(['error' => $xml_data['message']]);
+} else {
+    echo json_encode($xml_data);
+}
